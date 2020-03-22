f0815b3c0a6955c3ce9ce4e4d1c1b022fc5179d4
tonyeko <tonyeko@users.noreply.github.com>
2020-03-23 00:05:30 +0700

    lepas gitignore

--- test/ExceptionTest/ExceptionTest.pro
+++ test/ExceptionTest/ExceptionTest.pro
@@ -7,6 +7,15 @@ CONFIG += thread
 CONFIG -= qt
 
 HEADERS += \
+        Exception/BaseException.hpp \
+        Exception/DivideByZeroException.hpp \
+        Exception/DoubleNegationException.hpp \
+        Exception/EmptyMemoryException.hpp \
+        Exception/EmptyParenthesesException.hpp \
+        Exception/InvalidExpressionException.hpp \
+        Exception/NegativeSqrtException.hpp \
+        Exception/NullPointerException.hpp \
+        Exception/OperationFailedException.hpp \
         tst_exceptiontest.h
 
 SOURCES += \
--- test/ExceptionTest/tst_exceptiontest.h
+++ test/ExceptionTest/tst_exceptiontest.h
@@ -3,13 +3,61 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
+#include "Exception/BaseException.hpp"
+#include "Exception/DivideByZeroException.hpp"
+#include "Exception/DoubleNegationException.hpp"
+#include "Exception/EmptyParenthesesException.hpp"
+#include "Exception/InvalidExpressionException.hpp"
+#include "Exception/NegativeSqrtException.hpp"
+#include "Exception/NullPointerException.hpp"
+#include "Exception/OperationFailedException.hpp"
 
 using namespace testing;
+using namespace std;
 
-TEST(Exception, ExceptionTest)
+BaseException *exc;
+
+TEST(Exception, DivideByZeroException)
 {
-    EXPECT_EQ(1, 1);
-    ASSERT_THAT(0, Eq(0));
+    exc = new DivideByZeroException();
+    EXPECT_EQ(exc->getMessage(), "DIV BY ZERO");
 }
 
+TEST(Exception, DoubleNegationException)
+{
+    exc = new DoubleNegationException();
+    EXPECT_EQ(exc->getMessage(), "DOUBLE NEG");
+}
+
+TEST(Exception, EmptyParenthesesException)
+{
+    exc = new EmptyParenthesesException();
+    EXPECT_EQ(exc->getMessage(), "EMPTY ()");
+}
+
+TEST(Exception, InvalidExpressionException)
+{
+    string err = "EMPTY";
+    exc = new InvalidExpressionException(err);
+    EXPECT_EQ(exc->getMessage(), err);
+}
+
+TEST(Exception, NegativeSqrtException)
+{
+    exc = new NegativeSqrtException();
+    EXPECT_EQ(exc->getMessage(), "NEG SQRT");
+}
+
+TEST(Exception, NullPointerException)
+{
+    exc = new NullPointerException();
+    EXPECT_EQ(exc->getMessage(), "NULL");
+}
+
+TEST(Exception, OperationFailedException)
+{
+    exc = new OperationFailedException(new NullPointerException());
+    string comp = "ERR: NULL";
+    EXPECT_EQ(exc->getMessage(), comp);
+}
 #endif // TST_EXCEPTIONTEST_H
